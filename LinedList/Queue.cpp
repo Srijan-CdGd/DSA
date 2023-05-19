0992eabb3a19423d2f346c1aad28e2cee71b6c38
Srijan Airon <srijanairon27@gmail.com>
2023-05-19 21:13:23 +0530

    queue final commit

--- LinedList/Queue.cpp
+++ LinedList/Queue.cpp
@@ -3,7 +3,52 @@
 
 class Queue{
     private:
-        int start;
-        int end;
+        SinglyLinkedList* start = NULL;
+        SinglyLinkedList* end = NULL;
+        int length = 0;
     
+    public:
+        Queue(int val){
+            start = new SinglyLinkedList(val);
+            end = start->getTail();
+            length++;
+        }
+
+        void insert(int val){
+            start->insertAtEnd(val);
+            end = start->getTail();
+            length++;
+        }
+
+        int remove(){
+            int v = start->getNodeVal(length);
+            start->deleteFromStart();
+            length--;
+            return v;
+        }
+
+        int getLength(){
+            return length;
+        }
+
+        void print(){
+            start->print();
+        }
 };
+
+int main(){
+    Queue* q = new Queue(1);
+    q->insert(2);
+    q->insert(3);
+    q->insert(4);
+    q->print();
+    cout<<"Length of the current queue: "<<q->getLength()<<endl;
+    cout<<q->remove()<<endl;
+    q->print();
+    cout<<q->remove()<<endl;
+    q->print();
+    cout<<q->remove()<<endl;
+    q->print();
+    cout<<"Length of the current queue: "<<q->getLength()<<endl;
+    return 0;
+}
