de65849cb0753c1a39cb487de2743b5006546fe1
Srijan Airon <srijanairon27@gmail.com>
2023-06-26 18:44:43 +0530

    recursive bubble sort complete

---
+++ SortingAlgo/recursive_bubble_sort.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+# define fout(n, a) for(int i = 0; i < n; i++){cout<<a[i]<<" ";}
+# define fin(n, a) for(int i = 0; i < n; i++){cin>>a[i];}
+
+void bubble_sort(int*a, int i){
+    if(i == 0) return;
+    for(int j = i; j >= 0; j--){
+        if(*(a+i) < *(a+j)) swap(*(a+i), *(a+j));
+    }
+    bubble_sort(a, i-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    int a[n];
+
+    fin(n, a);
+    bubble_sort(a, n-1);
+    fout(n, a);
+    cout<<endl;
+
+    return 0;
+}
