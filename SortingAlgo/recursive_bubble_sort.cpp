5e882dd08874e6821192ee7f5d00d8020d584987
Srijan Airon <srijanairon27@gmail.com>
2023-06-26 18:48:39 +0530

    recursive bubble sort function correction

--- SortingAlgo/recursive_bubble_sort.cpp
+++ SortingAlgo/recursive_bubble_sort.cpp
@@ -4,12 +4,12 @@ using namespace std;
 # define fout(n, a) for(int i = 0; i < n; i++){cout<<a[i]<<" ";}
 # define fin(n, a) for(int i = 0; i < n; i++){cin>>a[i];}
 
-void bubble_sort(int*a, int i){
+void recursive_bubble_sort(int*a, int i){
     if(i == 0) return;
     for(int j = i; j >= 0; j--){
         if(*(a+i) < *(a+j)) swap(*(a+i), *(a+j));
     }
-    bubble_sort(a, i-1);
+    recursive_bubble_sort(a, i-1);
 }
 
 int main(){
@@ -19,7 +19,7 @@ int main(){
     int a[n];
 
     fin(n, a);
-    bubble_sort(a, n-1);
+    recursive_bubble_sort(a, n-1);
     fout(n, a);
     cout<<endl;
 
